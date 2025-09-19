a76b53bfec156b8dbcfcca26f6a634acd0362ce6
Denys Bokov <denys.bokov@jonasundderwolf.de>
2025-09-19 15:36:04 +0200

    feat(models): create new models and update movie model

--- cinema/models.py
+++ cinema/models.py
@@ -1,10 +1,39 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        ordering = ['last_name']
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=50)
+    rows = models.IntegerField()
+    seats_in_rows = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
 class Movie(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=50)
     description = models.TextField()
     duration = models.IntegerField()
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
 
     def __str__(self):
         return self.title
